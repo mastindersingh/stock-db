0de54986b95337cc38dfe9fcaf70448fe361416d
mastinder singh <mastinder@gmail.com>
2023-12-17 12:32:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from decimal import Decimal
 import pandas as pd
 import logging
 from dotenv import load_dotenv
-load_dotenv('.env.local')
+load_dotenv('.env')
 from flask_mail import Mail, Message
 
 
